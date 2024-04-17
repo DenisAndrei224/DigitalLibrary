85d2701d072c3f2f2783c1a9bdf52cb3eccd01f6
Denis Andrei Firulescu <firulescu.denis.314k@gmail.com>
2024-04-17 12:45:30 +0300

    Changed navbar so that it shows when I logged in

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  isLoggedIn!: boolean;
+  username: string | null = null;
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.userService.isLoggedIn().subscribe((loggedIn) => {
+      this.isLoggedIn = loggedIn;
+      if (loggedIn) {
+        this.userService.getUsername().subscribe((username) => {
+          this.username = username;
+        });
+      } else {
+        this.username = null;
+      }
+    });
   }
-
 }
